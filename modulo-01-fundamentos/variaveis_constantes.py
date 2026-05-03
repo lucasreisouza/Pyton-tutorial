7a370af9450e11f5fa2c32b2d58e43fab663113d
lucasreisouza <lulucasreisouza@gmail.com>
2026-05-03 19:02:13 -0300

    Variáveis e constantes
    Aprendi um pouco sobre variáveis e constantes, convenção, snake case

---
+++ modulo-01-fundamentos/variaveis_constantes.py
@@ -0,0 +1,14 @@
+nome = "Lucas" # Variável do tipo string
+idade = 28 # Variável do tipo inteiro
+
+nome = "Leonardo" # Reatribuindo o valor da variável nome
+
+print(nome, idade) # Imprime o valor das variáveis nome e idade
+
+limite_saque_diario = 1000 # Variável do tipo inteiro trabalhando a snake case
+ 
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"] # Variável do tipo lista, utilizando a convenção de letras maiúsculas para indicar que é uma constante
+
+BRAZILIAN_STATES = 10 # É possível atribuir um novo valor a uma constante, pois ainda é uma variável, sendo considerada uma constante por convenção. No entanto, isso não é recomendado, pois pode causar confusão e erros no código.
+
+print (BRAZILIAN_STATES)
